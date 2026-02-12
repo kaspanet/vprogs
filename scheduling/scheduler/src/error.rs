14103614e9288faeadd8e8c9cc41dad6c1ec062e
Hans Moog <3293976+hmoog@users.noreply.github.com>
2026-02-12 21:29:31 +0100

    refactored error types

--- scheduling/scheduler/src/error.rs
+++ scheduling/scheduler/src/error.rs
@@ -1,30 +1,11 @@
-use std::{
-    error::Error,
-    fmt,
-    fmt::{Display, Formatter},
-};
-
 /// Errors produced by the scheduler.
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, thiserror::Error)]
 pub enum SchedulerError {
     /// Pruning has advanced past the rollback target. The rollback pointers
     /// needed to restore state have been deleted.
+    #[error("pruning has advanced past the rollback target")]
     PruningConflict,
 }
-
-impl Display for SchedulerError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::PruningConflict => write!(
-                f,
-                "pruning has advanced past the rollback target: \
-                 rollback pointers needed to restore state have been deleted"
-            ),
-        }
-    }
-}
-
-impl Error for SchedulerError {}
 
 /// Convenience alias for results returned by the scheduler.
 pub type SchedulerResult<T> = Result<T, SchedulerError>;
